9b09d798a4cc3fcb6138e4f47496c6316bd4e704
Joseph Kamau <muigaijosephkamau@gmail.com>
2019-10-13 23:35:35 +0300

    updated the config_options

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,14 @@
 from flask import Flask
+from config import config_options
 
 #initialization of application
-app = Flask(__name__)
+def create_app(config_name):
+    app = Flask(__name__)
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+   # setting config
+    from .request import configure_request
+    configure_request(app)
+    
+    return app
