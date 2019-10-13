b7519d7d987eb230e22fe5d1dbd13e2f27a95c88
Joseph Kamau <muigaijosephkamau@gmail.com>
2019-10-14 01:13:00 +0300

    initialized the flask_bootstrap extension

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,22 @@
+
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from config import config_options
 
-#initialization of application
+bootstrap = Bootstrap()
+
 def create_app(config_name):
     app = Flask(__name__)
+    
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
-
-   # setting config
+    
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    
+  
+    
+    # setting config
     from .request import configure_request
     configure_request(app)
     
