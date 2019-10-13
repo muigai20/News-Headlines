58a924a31eedb667864fc7f5e19bef1079de8a94
Joseph Kamau <muigaijosephkamau@gmail.com>
2019-10-14 01:33:26 +0300

    created class source with the initialization method with it

--- app/models.py
+++ app/models.py
@@ -8,3 +8,16 @@ class Sources:
         self.description = description
         self.url = url
         self.category = category
+    
+    class News:
+    '''
+    News class to define News Objects
+    '''
+
+    def __init__(self, author, title, description, url, urlToImage, content):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.content = content
